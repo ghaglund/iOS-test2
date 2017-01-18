7af91e2746f461564102fee4d4bf8acb737cdb38
ghaglund <ghaglund@upsales.com>
2017-01-18 15:53:52 +0100

    Here is my first message

---
+++ iOS-test2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-test2
+//
+//  Created by Gabriel Haglund El Gaidi on 2017-01-18.
+//  Copyright © 2017 Gabriel Haglund El Gaidi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
