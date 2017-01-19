c8fb44c77ce8bcee7e1f6e65f349d2913be2eace
ghaglund <ghaglund@upsales.com>
2017-01-19 11:49:30 +0100

    Added new greeting lang tag

--- iOS-test2/ViewController.swift
+++ iOS-test2/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        displayLabel.text = NSLocalizedString("Welcome!", comment: "Welcome as a greeting")
     }
 
     override func didReceiveMemoryWarning() {
